98aaeefd9d547cbe0871837ad590158c9e0319c8
ivakhnenkoalex <you@example.comivakhnenko_alex@bk.ru>
2020-11-12 13:55:45 +0300

    ghghj

--- ft_join.py
+++ ft_join.py
@@ -10,28 +10,7 @@ def ft_join(lst, s = ' '):
     i = 0
     r = ''
     while d > i:
-        if lst[i] == 0:
-            r = r + '0'
-        elif lst[i] == 1:
-            r = r + '1'
-        elif lst[i] == 2:
-            r = r + '2'
-        elif lst[i] == 3:
-            r = r + '3'
-        elif lst[i] == 4:
-            r = r + '4'
-        elif lst[i] == 5:
-            r = r + '5'
-        elif lst[i] == 6:
-            r = r + '6'
-        elif lst[i] == 7:
-            r = r + '7'
-        elif lst[i] == 8:
-            r = r + '8'
-        elif lst[i] == 9:
-            r = r + '9'
-        else:
-            r = r + lst[i]
+        r = r + lst[i]
         if i != d - 1:
             r = r + s
         i = i + 1
